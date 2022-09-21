0d9d24f990ea1b77546f676b76f2f1ce0f77947a
Pixel <pixelgames3786@gmail.com>
2022-09-21 11:45:45 +0900

    BattleScene06

--- Assets/Script/Game/Battle/BattleManager.cs
+++ Assets/Script/Game/Battle/BattleManager.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class BattleManager : MonoBehaviour
 {
     //現在持っているマナコスト
-    public int HavingCost=3;
+    public int HavingCost = 3;
 
     private int TurnCount = 1;
 
@@ -16,12 +16,15 @@ public class BattleManager : MonoBehaviour
 
     public Transform HandField;
     public GameObject CardPrefab;
-    public TextMeshProUGUI TurnText,MyCostText,MyHPText,EnemyHPText;
+    public TextMeshProUGUI TurnText, MyCostText, MyHPText, EnemyHPText;
 
-    public BattleStatus MyChara, Enemy;
+    public BattleStatus MyChara = new BattleStatus(),
+                        Enemy = new BattleStatus();
 
     //カードを手札で管理する
-    public List<int> Deck,HandCard,ChoicedCard = new List<int>();
+    public List<int> Deck, HandCard, ChoicedCard = new List<int>();
+
+    private GameObject[] HandCardTrans=new GameObject[5];
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +40,7 @@ public class BattleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     //デュエルが始まった時
@@ -66,23 +69,51 @@ public class BattleManager : MonoBehaviour
         }
 
         //ドロー処理&カード生成
-        for (int i=0;i<5;i++)
+        for (int i = 0; i < 5; i++)
         {
             HandCard.Add(Deck[i]);
 
+            //カード生成
             CardController CreatedCard = Instantiate(CardPrefab, HandField).GetComponent<CardController>();
+
+            HandCardTrans[i]=CreatedCard.gameObject;
 
             //位置設定＆カード番号入力
             CreatedCard.CardNumber = Deck[i];
             CreatedCard.HandNumber = i;
 
-            CreatedCard.transform.localPosition = new Vector3(-500+i*250,0,0);
+            CreatedCard.transform.localPosition = new Vector3(-500 + i * 250, 0, 0);
 
             CreatedCard.BM = this;
         }
         //デッキからドローした分のカードを削除
         Deck.RemoveRange(0, 5);
 
+    }
+
+    //ドロー処理&カード生成
+    private void Draw(int[] Blank)
+    {
+        //ドロー処理&カード生成
+        for (int i = 0; i < 5; i++)
+        {
+            HandCard.Add(Deck[i]);
+
+            //カード生成
+            CardController CreatedCard = Instantiate(CardPrefab, HandField).GetComponent<CardController>();
+
+            HandCardTrans[i] = CreatedCard.gameObject;
+
+            //位置設定＆カード番号入力
+            CreatedCard.CardNumber = Deck[i];
+            CreatedCard.HandNumber = i;
+
+            CreatedCard.transform.localPosition = new Vector3(-500 + i * 250, 0, 0);
+
+            CreatedCard.BM = this;
+        }
+        //デッキからドローした分のカードを削除
+        Deck.RemoveRange(0, 5);
     }
 
     //ターンエンド処理
@@ -94,6 +125,7 @@ public class BattleManager : MonoBehaviour
     //ターンカウント増やしたりドローしたり
     public void TurnChange()
     {
+
         TurnCount++;
 
         TurnText.text = TurnCount.ToString();
@@ -112,11 +144,22 @@ public class BattleManager : MonoBehaviour
             //とりあえずダメージ与える処理のみ
             Enemy.HP -= Card.Power;
 
-            EnemyHPText.text = Enemy.HP.ToString();
+            EnemyHPText.text = Enemy.HP.ToString()+"<size=45>/"+Enemy.MaxHP.ToString()+"</size>";
 
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.3f);
         }
 
+        //手札から削除
+        foreach (int Num in ChoicedCard)
+        {
+            HandCard.RemoveAt(Num);
+            Destroy(HandCardTrans[Num].gameObject);
+        }
+        ChoicedCard.Clear();
+
+        yield return new WaitForSeconds(0.3f);
+
+        //敵の行動に移る
         StartCoroutine("EnemyCharaMove");
 
         yield return null;
